dfe5a2bb89c91ee96a83081dc835449e4bead7a2
kevinspr12-lang <kevinspr12@gmail.com>
2026-03-17 17:08:29 -0700

    Define models for blog application
    Added models for category, post, and comment in manage.py.

--- dj_blog/manage.py
+++ dj_blog/manage.py
@@ -1,22 +1,57 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from django.db import models
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_blog.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+# Create your models here.
+class category (models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField()
 
 
-if __name__ == '__main__':
-    main()
+    class Meta:
+        ordering = ('title')
+        verbose_name_plural = 'Categories'
+
+
+    def __str__(self):
+        return self.title
+   
+    def get_absolute_url(self):
+         return '/%s/' % self.slug  
+   
+class post(models.Mode):
+
+
+        ACTIVE= 'active'
+        DRAFT= 'draft'
+
+
+        CHOICES_STATUS = {
+            (ACTIVE, 'active'),
+            (DRAFT, 'draft')
+        }
+
+
+        category = models.ForeignKey(category, related_name='post', on_delete=models.CASCADE)
+        title = models.CharField(max_length=255)
+        intro = models.TextField()
+        body = models.TextField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
+        image = models.ImageField(upload_to='upload/', blank=True, null=True)
+
+
+        def __str__(self):
+            return self.title
+       
+class Comment (models.Mode):
+    post = models.ForeignKey(post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
+
+
