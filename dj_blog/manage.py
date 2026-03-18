286841961a81857641da8bec08918dd5e94823b2
kevinspr12-lang <kevinspr12@gmail.com>
2026-03-17 17:23:47 -0700

    Refactor models in manage.py for consistency
    Refactor Category and Post models, correcting typos and improving structure.

--- dj_blog/manage.py
+++ dj_blog/manage.py
@@ -1,37 +1,27 @@
 from django.db import models
 
-
-# Create your models here.
-class category (models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField()
-
+#create yours models here.
+class Category(models.Model):
+    tittle = models.CharField(max_length=255)
 
     class Meta:
         ordering = ('title')
         verbose_name_plural = 'Categories'
 
-
     def __str__(self):
-        return self.title
+        return self.tittle
    
-    def get_absolute_url(self):
-         return '/%s/' % self.slug  
-   
-class post(models.Mode):
-
+    class post(models.Mode):
 
         ACTIVE= 'active'
         DRAFT= 'draft'
-
 
         CHOICES_STATUS = {
             (ACTIVE, 'active'),
             (DRAFT, 'draft')
         }
 
-
-        category = models.ForeignKey(category, related_name='post', on_delete=models.CASCADE)
+        Category = models.ForeignKey(Category, related_name='post', on_delete=models.CASCADE)
         title = models.CharField(max_length=255)
         intro = models.TextField()
         body = models.TextField()
@@ -39,19 +29,16 @@ class post(models.Mode):
         status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVE)
         image = models.ImageField(upload_to='upload/', blank=True, null=True)
 
-
         def __str__(self):
             return self.title
        
-class Comment (models.Mode):
-    post = models.ForeignKey(post, related_name='comments', on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    email = models.EmailField()
-    body = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    class Comment (models.Mode):
+        post = models.ForeignKey(post, related_name='comments', on_delete=models.CASCADE)
+        name = models.CharField(max_length=255)
+      
+  email = models.EmailField()
+        body = models.TextField()
+        created_at = models.DateTimeField(auto_now_add=True)
 
-
-    def __str__(self):
-        return self.name
-
-
+        def __str__(self):
+            return self.name
